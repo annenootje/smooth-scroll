be5b579984460d0b37af5d28fc756048f8730772
Anne Noteboom <annenootje@live.nl>
2021-10-29 10:41:50 +0200

    Update smooth-scroll.js

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,19 +1,23 @@
+// Smooth scroll
+
 const links = document.querySelectorAll('a[href^="#"]');
 
 if (links.length) {
-    links.forEach(link => {
+    links.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        const id = document.querySelector(link.hash);
+        const offset = 120;
+        const target = id.offsetTop - offset;
 
-        link.addEventListener('click', (e) => {
-            const id = document.querySelector(link.hash);
-            const offset = 50; // Set to your own wishes
-            const target = id.getBoundingClientRect().top - offset;
-        
-            window.scroll({
-                top: target,
-                behavior: 'smooth'
-            });
-        
-            e.preventDefault();
+        console.log(id);
+        console.log(target);
+
+        window.scroll({
+          top: target,
+          behavior: "smooth",
         });
+
+        e.preventDefault();
+      });
     });
 }
